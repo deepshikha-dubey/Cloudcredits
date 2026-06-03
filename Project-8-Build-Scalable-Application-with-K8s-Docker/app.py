7599eb78f6d8c90248a2539fc01640b38d7f5be2
Deepshikha Dubey <140066430+deepshikha-dubey@users.noreply.github.com>
2026-06-03 18:18:49 +0530

    Update app.py

--- Project-8-Build-Scalable-Application-with-K8s-Docker/app.py
+++ Project-8-Build-Scalable-Application-with-K8s-Docker/app.py
@@ -2,8 +2,8 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "Hello from Flask + Docker + Kubernetes!"
+def home():
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
